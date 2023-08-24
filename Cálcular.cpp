83ff950f76be2ce144aa928939177b739f76bb0d
João <joaohdn4546@gmail.com>
2023-08-24 06:43:31 -0300

    commit att Férias

--- Cálcular.cpp
+++ Cálcular.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <string>
+#include <locale>
 
 using namespace std;
 
 int main(){
+
+ locale brLocale("pt_BR.UTF-8");
+ locale::global(brLocale);
+
 float resultado, salario, hora, valorhora, jornadamulti;
-int jornada, insalubridade;
+float jornada, insalubridade, ferias, inss;
+float ir, baseir, feriasatt;
 int button;
 
 do{
 cout << "o que deseja Calcular? " << endl;
 cout << "1 - Adicional Noturno" << endl;
 cout << "2 - Adicional de Insalubridade" << endl;
-cout << "3 -  Adicional de Periculosidade" << endl;
-cout << "4 - Sair" << endl;
+cout << "3 - Adicional de Periculosidade" << endl;
+cout << "4 - Cálcular Férias" << endl;
+cout << "5 - Sair" << endl;
 cin >> button;
 
 switch(button){
@@ -67,6 +74,44 @@ cout << "O adicional Ć© de: " << adctotal << " Somados ao salĆ�rio do indiv
 break;
 
 case 4:
+cout << "Qual o salário do Indivíduo?" << endl;
+cin >> salario;
+ferias = salario + (salario / 3);
+//inss
+if (ferias >= 7507.49){
+   inss = 876.95;
+}else if(ferias < 7507.49 && ferias >= 3856.95){
+   inss = salario - 3856.95 * 0.14 + (99.00 + 112.61);
+}else if(ferias < 3856.95 && ferias >= 2571.30){
+   inss = salario - 2571.30 * 0.12 + 99.00;
+}else if(ferias < 2571.30 && ferias > 1320.00){
+   inss = salario - 1320 * 0.09 + 99.00; 
+}else{
+   inss = 99.00;
+}
+//ir
+baseir = ferias - inss;
+if (baseir > 4664.69){
+   ir = baseir * 0.275 - 884.96;
+}else if(baseir > 3751.07 && baseir <= 4664.68){
+   ir = baseir * 0.225 - 651.73;
+}else if(baseir > 2826.66 && baseir <= 3751.06){
+   ir = baseir * 0.15 - 370.40;
+}else if(baseir > 2112.00 && baseir <= 2826.66){
+   ir = baseir * 0.075 - 158.40;
+}else{
+   ir = 0;
+}
+feriasatt = ferias - inss - ir;
+cout << "As férias do indivíduo serão de: " << feriasatt << endl;
+cout << "Sendo o INSS de: " << inss << endl;
+cout << "Sendo o IR de: " << ir << endl;
+
+
+
+break;
+
+case 5:
 cout << "Saindo..." << endl;
 return 0;
 break;
@@ -78,13 +123,6 @@ cout << " Selecione a opĆ§Ć£o desejada: " << endl;
 
 
 }
-}while(button != 4);
-
-
-
-
-
-
-
+}while(button != 5);
 
 }
